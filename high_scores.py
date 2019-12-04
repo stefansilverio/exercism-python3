c2740982d5ecc9ae325ee53ecb48dbe8b88ef189
stefansilverio <stefansilverio@gmail.com>
2019-12-04 11:57:39 -0500

    frogger component

--- high_scores.py
+++ high_scores.py
@@ -2,13 +2,10 @@
 # create high score component for frogger
 
 def latest(scores):
-    return scores.pop()
+    return scores.copy().pop()
 
 def personal_best(scores):
     return max(scores)
 
 def personal_top_three(scores):
-    if len(scores) >= 3:
-        return [scores.pop(scores.index(max(scores))) for i in range(3)]
-    else:
-        return [scores.pop(scores.index(max(scores))) for i in range(len(scores))]
+    return sorted(scores, reverse=True)[:3]
